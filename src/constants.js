bfeae48fbf86a079e60ceab9af9f982b10050bcf
kislay-gupta <kislaygupta007@gmail.com>
2024-04-17 23:17:06 +0530

    feat: added camera and player movement

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,22 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in NextJs with JavaScript/TypeScript .
+    I'm a front-end developer transitioning to fullstack developer. I also like  Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/kislay-gupta" target="_blank">Github</a>!`,
+  "cs-degree": `This is my BCA degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+  `,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@chaiaurcode" target="_blank">Chai aur Code
+   </a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+  <a href="https://www.youtube.com/@HiteshChoudharydotcom" target="_blank">Hitesh Choudhary 
+  </a> (sometimes) and <a href="https://www.youtube.com/@NetNinja/" target="_blank">Net Ninja</a> (sometimes)!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="#" target="_blank">Check it out?</a>
+  Contact me at kislaygupta007@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
+  library: `adding list of project soon`,
+  exit: `If you want to exit Kislay's portfolio, just close the tab.`,
+};
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,8 @@
-export function dialogueDisplay(text, onDisplayEnd) {
+export function displayDialogue(text, onDisplayEnd) {
   const dialogueUI = document.getElementById("textbox-container");
   const dialogue = document.getElementById("dialogue");
 
   dialogueUI.style.display = "block";
-
   let index = 0;
   let currentText = "";
   const intervalRef = setInterval(() => {
@@ -13,10 +12,12 @@ export function dialogueDisplay(text, onDisplayEnd) {
       index++;
       return;
     }
+
     clearInterval(intervalRef);
-  }, 5);
+  }, 1);
 
   const closeBtn = document.getElementById("close");
+
   function onCloseBtnClick() {
     onDisplayEnd();
     dialogueUI.style.display = "none";
@@ -24,5 +25,21 @@ export function dialogueDisplay(text, onDisplayEnd) {
     clearInterval(intervalRef);
     closeBtn.removeEventListener("click", onCloseBtnClick);
   }
+
   closeBtn.addEventListener("click", onCloseBtnClick);
+
+  addEventListener("keypress", (key) => {
+    if (key.code === "Enter") {
+      closeBtn.click();
+    }
+  });
+}
+
+export function setCamScale(k) {
+  const resizeFactor = k.width() / k.height();
+  if (resizeFactor < 1) {
+    k.camScale(k.vec2(1));
+  } else {
+    k.camScale(k.vec2(1.5));
+  }
 }

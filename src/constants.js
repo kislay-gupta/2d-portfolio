20b6d30cfeb4af84a7365dd53e7470df6a8a8d55
kislay-gupta <kislaygupta007@gmail.com>
2024-08-01 20:10:15 +0530

    chore: added project

--- src/constants.js
+++ src/constants.js
@@ -17,6 +17,6 @@ export const dialogueData = {
   Contact me at kislaygupta007@gmail.com if you have any interesting job opportunities!`,
   projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
   Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. `,
-  library: `adding list of project soon`,
+  library: `<a href="next-miro.vercel.app" target="_blank">Miro Clone</a>`,
   exit: `If you want to exit Kislay's portfolio, just close the tab.`,
 };
